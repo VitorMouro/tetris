3aa8c7c8c4f372d98f694ce394cabbcd68d6cbbf
Vitor Mouro <vitor.a.mouro@gmail.com>
2019-12-23 19:14:06 -0300

    moving and rotating pieces

--- js/Piece.js
+++ js/Piece.js
@@ -55,7 +55,7 @@ class Piece {
             'cyan'
         ];
         this.id = Math.floor(Math.random() * 7);
-        this.blocks = this.pieces[this.id];
+        this.config = this.pieces[this.id];
         this.color = this.colors[this.id];
         this.position = {
             x: 0,
@@ -64,27 +64,193 @@ class Piece {
 
         for (let i = 0; i < 5; i++)
             for (let j = 0; j < 5; j++)
-                if (this.blocks[i][j] == 1) {
-                    this.blocks[i][j] = new Block(this.color, this.position.x + i, this.position.y + j, 1, 1);
+                if (this.config[i][j] == 1) {
+                    this.config[i][j] = new Block(this.color, this.position.x + i, this.position.y + j, 1, 1);
                 } else {
-                    this.blocks[i][j] = null;
+                    this.config[i][j] = null;
                 }
 
     }
 
+    rotateCounterClockwise(a) {
+        var n = a.length;
+        for (var i = 0; i < n / 2; i++) {
+            for (var j = i; j < n - i - 1; j++) {
+                var tmp = a[i][j];
+                a[i][j] = a[j][n - i - 1];
+                a[j][n - i - 1] = a[n - i - 1][n - j - 1];
+                a[n - i - 1][n - j - 1] = a[n - j - 1][i];
+                a[n - j - 1][i] = tmp;
+            }
+        }
+        return a;
+    }
+
+    rotateClockwise(a) {
+        var n = a.length;
+        for (var i = 0; i < n / 2; i++) {
+            for (var j = i; j < n - i - 1; j++) {
+                var tmp = a[i][j];
+                a[i][j] = a[n - j - 1][i];
+                a[n - j - 1][i] = a[n - i - 1][n - j - 1];
+                a[n - i - 1][n - j - 1] = a[j][n - i - 1];
+                a[j][n - i - 1] = tmp;
+            }
+        }
+        return a;
+    }
+
+    rotate() {
+        this.rotateClockwise(this.config);
+        for (let i = 0; i < 5; i++)
+            for (let j = 0; j < 5; j++)
+                if (this.config[i][j]) {
+                    this.config[i][j].position.x = (this.position.x + i) * cellSize;
+                    this.config[i][j].position.y = (this.position.y + j) * cellSize;
+                }
+    }
+
+    unrotate() {
+        this.rotateCounterClockwise(this.config);
+        for (let i = 0; i < 5; i++)
+            for (let j = 0; j < 5; j++)
+                if (this.config[i][j]) {
+                    this.config[i][j].position.x = (this.position.x + i) * cellSize;
+                    this.config[i][j].position.y = (this.position.y + j) * cellSize;
+                }
+    }
+
+    canRotate() {
+        this.rotateClockwise(this.config);
+        for (let i = 0; i < 5; i++)
+            for (let j = 0; j < 5; j++)
+                if (this.config[i][j] && (this.position.x + i) >= 0 && (this.position.y + j) >= 0)
+                    if (board.grid[this.position.x + i][this.position.y + j].color != 'transparent') {
+                        this.rotateCounterClockwise(this.config);
+                        return false;
+                    }
+        this.rotateCounterClockwise(this.config);
+        return true;
+    }
+
+    canMove(dir) {
+        if (dir == 'right') {
+            for (let i = 0; i < 5; i++)
+                for (let j = 0; j < 5; j++)
+                    if (this.config[i][j] && board.grid[this.position.x + i + 1][this.position.y + j].color != 'transparent')
+                        return false;
+            return true;
+        }
+        if (dir == 'left') {
+            for (let i = 0; i < 5; i++)
+                for (let j = 0; j < 5; j++)
+                    if (this.config[i][j] && board.grid[this.position.x + i - 1][this.position.y + j].color != 'transparent')
+                        return false;
+            return true;
+        }
+        if (dir == 'down') {
+            for (let i = 0; i < 5; i++)
+                for (let j = 0; j < 5; j++)
+                    if (this.config[i][j] && board.grid[this.position.x + i][this.position.y + j + 1].color != 'transparent')
+                        return false;
+            return true;
+        }
+    }
+
+    move(dir) {
+        if (dir == 'right') {
+            this.position.x += 1;
+            for (let i = 0; i < 5; i++)
+                for (let j = 0; j < 5; j++)
+                    if (this.config[i][j]) {
+                        this.config[i][j].position.x = (this.position.x + i) * cellSize;
+                    }
+        } else if (dir == 'left') {
+            this.position.x -= 1;
+            for (let i = 0; i < 5; i++)
+                for (let j = 0; j < 5; j++)
+                    if (this.config[i][j]) {
+                        this.config[i][j].position.x = (this.position.x + i) * cellSize;
+                    }
+        } else if (dir == 'down') {
+            this.position.y += 1;
+            for (let i = 0; i < 5; i++)
+                for (let j = 0; j < 5; j++)
+                    if (this.config[i][j]) {
+                        this.config[i][j].position.y = (this.position.y + j) * cellSize;
+                    }
+        }
+    }
+
     update(dir) {
-        if(dir == 'down')
-            console.log('Down');
-        else if(dir == 'right')
-            console.log('Right');
-        else if(dir == 'left')
-            console.log('Left');
+        if (dir == 'right' && this.canMove(dir)) {
+            this.move(dir);
+        }
+        else if (dir == 'left' && this.canMove(dir)) {
+            this.move(dir);
+        }
+        else if (dir == 'down' && this.canMove(dir)) {
+            this.move(dir);
+        }
+        else if (dir == 'space') {
+            if (this.id != 1)
+                if (this.canRotate())
+                    this.rotate();
+                else {
+                    if (this.canMove('right')) {
+                        this.move('right');
+                        if (this.canRotate()) {
+                            this.rotate();
+                            return;
+                        } else {
+                            this.move('left');
+                        }
+                    } else if (this.canMove('left')) {
+                        this.move('left');
+                        if (this.canRotate()) {
+                            this.rotate();
+                            return;
+                        } else {
+                            this.move('right');
+                        }
+                    }
+                    if (this.canMove('right')) {
+                        this.move('right');
+                        if (this.canMove('right')) {
+                            this.move('right');
+                            if (this.canRotate())
+                                this.rotate();
+                            else {
+                                this.move('left');
+                                this.move('left');
+                            }
+                        } else {
+                            this.move('left');
+                        }
+                    } else if (this.canMove('left')) {
+                        this.move('left');
+                        if (this.canMove('left')) {
+                            this.move('left');
+                            if (this.canRotate())
+                                this.rotate();
+                            else {
+                                this.move('right');
+                                this.move('right');
+                            }
+                        } else {
+                            this.move('right');
+                        }
+                    }
+
+                }
+
+        }
     }
 
     draw(ctx) {
         for (let i = 0; i < 5; i++)
             for (let j = 0; j < 5; j++)
-                if (this.blocks[i][j] != null)
-                    this.blocks[i][j].draw(ctx);
+                if (this.config[i][j] != null)
+                    this.config[i][j].draw(ctx);
     }
 }
--- js/input.js
+++ js/input.js
@@ -1,14 +1,21 @@
 document.addEventListener('keydown', keyDownHandler, false);
 
 function keyDownHandler(event) {
-    console.log(event.keyCode)
+    // console.log(event.keyCode)
     if (event.keyCode == 39) {
         rightPressed = true;
+        console.log('Right');
     }
     else if (event.keyCode == 37) {
         leftPressed = true;
+        console.log('Left');
     }
     if (event.keyCode == 40) {
         downPressed = true;
+        console.log('Down');
+    }
+    if (event.keyCode == 32) {
+        spacePressed = true;
+        console.log('Rotate');
     }
 }
